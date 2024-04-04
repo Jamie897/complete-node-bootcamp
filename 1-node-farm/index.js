ec2b100f5a33f4719e3cd3cc9f8f2032618861a0
Jamie897 <jamiedanielle456@gmail.com>
2024-04-04 10:33:15 -0500

    Synchronous

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
+const input = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(input);
 
 const textOut = 'This is what we know about the avocado: $(textIn).\nCreated on $(Date.now)';
 fs.writeFileSync('./txt/output.txt', textOut);
